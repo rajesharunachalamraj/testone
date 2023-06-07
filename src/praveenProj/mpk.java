96587c02cff546e55c8d4bfe670aecab33f9732a
RajeshArunachalam <rajeshdevops.24.04.93@gmail.com>
2023-06-07 15:14:51 +0530

    testone sample

---
+++ src/praveenProj/mpk.java
@@ -0,0 +1,10 @@
+package praveenProj;
+
+public class mpk {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
---
+++ src/praveenProj/mpkprogram.java
@@ -0,0 +1,10 @@
+package praveenProj;
+
+public class mpkprogram {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
